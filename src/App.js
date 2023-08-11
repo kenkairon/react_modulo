7d412203b2614074c472865df50720537d3e0fc7
kenkairon <ingeinfo2006@gmail.com>
2023-08-10 23:39:47 -0400

    Agregar boton de edad y sumarle 1 al apretar el boton

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import GreetingF from './components/pure/greeting';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        HOLA MUNDO
-        </p>
-    
+        {/* Componente propio de Greeting.jsx */}
+        {/*<Greeting name="Carlos"></Greeting>*/}
+        <GreetingF name="Carlos"></GreetingF>
       </header>
     </div>
   );
